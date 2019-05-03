2f3dcc70c56c53d6ef1eaeb235f062f08ee1f4ae
backyeseul@naver.com <backyeseul@naver.com>
2019-05-03 15:35:26 +0900

    포트번호 실행후 입력

--- src/test/TCPServerTest.java
+++ src/test/TCPServerTest.java
@@ -1,17 +1,15 @@
 package test;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 //system.out.println
 public class TCPServerTest {
 	
 	
 	// 源�寃쏀썕 �닔�젙.
-	private static final int PORT = 8000;
+	private static int PORT = 8005;
 	//////////////
 	public static GameManager gameManager = new GameManager();
 	///
@@ -27,7 +25,9 @@ public class TCPServerTest {
 		game = new Game ("Room 3", 4);
 		gameManager.addGame(game);
 		
-		
+		Scanner sc = new Scanner (System.in);
+		System.out.print ("Enter port number : ");
+		PORT = sc.nextInt();
 		
 			ServerSocket serverSocket = null;
 			try {
